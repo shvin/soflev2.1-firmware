22f9da27768fbe08afe1160e45df6f705f7b1d4f
ashvin <ashvincodes@gmail.com>
2023-03-31 17:23:39 -0400

    rgb update

--- config.h
+++ config.h
@@ -1,37 +1,17 @@
 #pragma once
 
-/* The way how "handedness" is decided (which half is which),
-see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
-for more options.
-*/
-
-#pragma once
-
 #define MASTER_LEFT
-#define OLED_TIMEOUT 90000
 
-/* #ifdef RGBLIGHT_ENABLE
-#define RGBLIGHT_LIMIT_VAL 120
-#define RGBLIGHT_SPLIT
-    // Disabling some of these is a good way to save flash space.
-//  #define RGBLIGHT_EFFECT_ALTERNATING
-//  #define RGBLIGHT_EFFECT_RGB_TEST
-//  #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-//  #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#ifdef RGBLIGHT_ENABLE
+    #define RGB_DI_PIN D3
+    #define RGBLED_NUM 14
+    #define RGBLED_SPLIT { 7, 7 }
+    #define RGBLIGHT_ANIMATIONS
+    #define RGBLIGHT_HUE_STEP 8
+    #define RGBLIGHT_SAT_STEP 8
+    #define RGBLIGHT_VAL_STEP 8
     #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-//  #define RGBLIGHT_EFFECT_BREATHING
-//  #define RGBLIGHT_EFFECT_KNIGHT
-//  #define RGBLIGHT_EFFECT_SNAKE
-//  #define RGBLIGHT_EFFECT_CHRISTMAS
-//  #define RGBLIGHT_EFFECT_TWINKLE
-
-    // Set default lighting option
-    #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_SWIRL+2
+    #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_SWIRL
+    #define RGBLIGHT_SLEEP
     #define RGBLIGHT_LAYERS
-
-    // Allow compiling to work on sofle/rev1
-    #if defined(KEYBOARD_sofle_rev1)
-	    #define RGBLED_NUM 70
-	    #define RGB_DI_PIN D3
-    #endif
-#endif */
+#endif
